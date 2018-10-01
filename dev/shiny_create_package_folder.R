851e399719dd28c7e3a0d8357072bcb85806b50b
Unknown <timat35@gmail.com>
2018-10-01 14:59:58 +0200

    clean repo

--- dev/shiny_create_package_folder.R
+++
@@ -1,5 +0,0 @@
-.libPaths("c:/csu_shiny/pkg")
-
-install.packages("C:/Projects/Rcan/Rcan_1.3.1.tar.gz", repos=NULL)
-
-library(data.table)
--- dev/shiny_start.R
+++
@@ -1,7 +0,0 @@
-shiny_folder <- "C:/Projects/Rcan/Shiny"
-.libPaths(paste0(shiny_folder,"/pkg")
-
-library(shiny)
-
-shiny_folder <- "C:/Projects/Rcan/Shiny"
-shiny::runApp(shiny_folder, launch.browser = TRUE)
