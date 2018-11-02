23891ecfcf02789dfa217cc3445a51ed825dea36
Unknown <timat35@gmail.com>
2018-11-02 10:37:48 +0100

    canreg build

--- dev/Rcan_script.R
+++ dev/Rcan_script.R
@@ -23,7 +23,7 @@ test_dir("C:/Projects/Rcan/Rcan/tests/testthat")
 library(devtools)
 
 check("C:/Projects/Rcan/Rcan") #local check
-build("C:/Projects/Rcan/Rcan", manual = TRUE) # build package
+build("C:/Projects/Rcan/Rcan",path="C:/Projects/canreg5/conf/tables/r/r-packages", manual = TRUE) # build package
 
 # post package on CRAN
 
