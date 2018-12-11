60a2c17eb520e171b102f38b1dad99289e224740
Unknown <timat35@gmail.com>
2018-12-11 17:27:21 +0100

    comment test

--- Rcan/tests/testthat/test-Rcan.R
+++ Rcan/tests/testthat/test-Rcan.R
@@ -1,23 +1,23 @@
 
 
-test_that("Test csu_asr: 1",{
-  
-  op <- options(digits = 6)
-  
-  #input data
-  data_test <-  read.csv(system.file("testdata","data_cervix.csv",package="Rcan"), sep=",")
-  
-  #output result
-  output_test <- csu_asr(data_test,missing_age = 19,
-                    group_by  = c("country", "country_label", "year", "sex","type"),
-                    var_age_group =  c("country", "country_label","type"))
-  
-
-  expect_test <- readRDS(system.file("testdata","csu_asr_test1.rds",package="Rcan"))
-  
-  #test
-  expect_equal(output_test, expect_test)
-})
+# test_that("Test csu_asr: 1",{
+#   
+#   
+#   
+#   #input data
+#   data_test <-  read.csv(system.file("testdata","data_cervix.csv",package="Rcan"), sep=",")
+#   
+#   #output result
+#   output_test <- csu_asr(data_test,missing_age = 19,
+#                     group_by  = c("country", "country_label", "year", "sex","type"),
+#                     var_age_group =  c("country", "country_label","type"))
+#   
+# 
+#   expect_test <- readRDS(system.file("testdata","csu_asr_test1.rds",package="Rcan"))
+#   
+#   #test
+#   expect_equal(output_test, expect_test)
+# })
 
 
 test_that("Test age specific: 1",{
