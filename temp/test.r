84fc87803aced193ab3a122d3e65ca7ce50717ed
laversanne <timat35@gmail.com>
2019-03-28 14:25:52 +0100

    update test

--- temp/test.r
+++ temp/test.r
@@ -7,8 +7,21 @@ library(data.table)
 
 #install.packages("rlang")
 
+dat_ind <- read.table("C:/Projects/Rcan/temp/AFSOUTRA_cc_end.txt", header = TRUE, sep = "|")
+
+dat <- csu_group_cases(dat_ind, var_age = "age",
+                       group_by = c("sex", "regcode", "icd10","ses")
 
 
+
+dat_ind$reglabel <- "Eastern Cape, South Africa"
+dat <- csu_group_cases(dat_ind, var_age = "age",
+                       group_by = c("sex", "regcode", "icd10","reglabel"))
+
+
+dat4 <- csu_group_cases(dat_ind, var_age = "age",
+                        group_by = c("sex", "regcode"),
+                        var_year = "doi")
 
 
 source("C:/Projects/Rcan/temp/function.r")
@@ -18,23 +31,24 @@ source("C:/Projects/Rcan/temp/function.r")
 data(ICD_group_file)
 data(data_individual_file)
 
+df_ICD <- read.csv("C:/Projects/Rcan/temp/Data-icd_test.csv")
+
 df_data <- data_individual_file
 var_age <- "age"
 group_by <- c("sex", "regcode", "reglabel")
 var_year <- "doi"
 
-df_ICD <- NULL
-var_ICD <- NULL
+var_ICD <- "site"
 var_cases <- NULL
 
 
 dt_test <- csu_group_cases(data_individual_file, var_age = "age",
                         group_by = c("sex", "regcode","reglabel"),
-                       	df_ICD = ICD_group_file,
+                       	df_ICD = df_ICD,
 						var_ICD  ="site",
                         var_year = "doi")
 
-
+dt_test[1:5,]
 
 
 
