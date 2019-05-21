fe76fd4a8cc5241237b33f6a669955d876803c28
laversanne <timat35@gmail.com>
2019-05-21 11:39:39 +0200

    re ifx

--- Rcan/R/csu_merge_cases_pop.R
+++ Rcan/R/csu_merge_cases_pop.R
@@ -16,7 +16,7 @@ csu_merge_cases_pop <- function(df_cases,df_pop, var_age,var_cases="cases",var_p
 
   if (!is.null(var_py)) {
     
-    core.error_variable(df_cases, var_py, csu_merge_cases_pop)
+    core.error_variable(df_pop, var_py, csu_merge_cases_pop)
 
     setnames(dt_pop, var_py, "CSU_P")
     for (colyear_pop in colnames(dt_pop)[!colnames(dt_pop) %in% c(var_age,"CSU_P")]) {
--- temp/test.r
+++ temp/test.r
@@ -3,6 +3,23 @@ remove.packages("Rcan")
 devtools::install_github("timat35/Rcan", ref = "dev", subdir="Rcan")
 
 
+
+#jermoe test 
+
+dcas <- read.csv("C:/Projects/Rcan/temp/Exercise2_qui.csv")
+dpop <- read.csv("C:/Projects/Rcan/temp/200612_population.csv")
+
+data("ICD_group_file")
+dcases <- csu_group_cases(dcas, var_age = "age", group_by = c("sex"),df_ICD = ICD_group_file, var_ICD = "site", var_year = "doi")
+
+names(dpop)[names(dpop) == 'agegrp'] <- 'age_group'
+names(dcases)
+
+df_cases <- dcases
+
+
+dcaspop <- csu_merge_cases_pop(df_cases, dpop, var_age = "age_group", var_cases = "cases", var_py = "pop", group_by = c("sex", "LABEL", "year"))
+
 #note population change to 85+
 #missing pop MUST BE ZERO
 
@@ -15,9 +32,18 @@ data(ICD_group_file)
 
 ICD_group_file
 getwd()
-write.csv(ICD_group_file, "temp.csv")
+write.csv(ICD_group_file, )
+ICD_group_file <- read.csv("C:/Projects/Rcan/temp/ICD_globocan.csv")
+save(ICD_group_file, file = "C:/Projects/Rcan/Rcan/data/ICD_group_file.rda")
+
+ICD_group_CI5XI <- read.csv("C:/Projects/Rcan/temp/ICD_CI5XI.csv")
+save(ICD_group_CI5XI, file = "C:/Projects/Rcan/Rcan/data/ICD_group_CI5XI.rda")
+
+
 setwd("C:/Projects/Rcan")
 source("temp/function.r")
+
+
 
 ## add function for population 
 
