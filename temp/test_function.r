57cec5377a0425abb4e3b4048b6e5c753c5a5b26
laversanne <timat35@gmail.com>
2019-05-22 14:04:05 +0200

    bar chart function
    need help file and example grrr

--- temp/test_function.r
+++ temp/test_function.r
@@ -4,12 +4,12 @@
 
 
 #DATA TABLE is DATA TABLE AT THE BEGINNING
-#CSU_ASR REGROUP
 
 
 library(Rcan)
 library(data.table)
 library(ggplot2)
+library(scales)
 
 setwd("C:/Projects/Rcan/temp")
 source("function.r")
@@ -31,6 +31,8 @@ dcaspop <- csu_merge_cases_pop(dcases, dpop, var_age = "age_group", var_cases = 
 str(dcaspop)
 table(dcaspop[,c("age_group")])
 
+dcaspop <- as.data.table(dcaspop)
+
 
 
 dcasasr <- csu_asr(dcaspop,
@@ -45,19 +47,49 @@ dcasasr <- as.data.table(dcasasr)
 dcasasr <- dcasasr[!LABEL %in% c("Other", "Other skin"), ]
 dcasasr <- dcasasr[!LABEL %in% c("Other", "Other skin"), ]
 
-
-dcasasr[, sex:=factor(sex, levels=c(1,2), labels=c("Male", "Female"))] ## need factor
-
+#casasr[, sex:=factor(sex, levels=c(1,2), labels=c("Male", "Female"))] ## need factor
+#ength(levels(dcasasr$sex))
 
 source("function.r")
 data <- csu_bar_top(
    dcasasr,
    var_top="asr",
    var_bar="LABEL",
-   group_by="sex") 
+   group_by="sex", 
+   nb_top = 10,
+   plot_title = "Top 10 cancer sites",
+   ytitle= "Number of cases",
+   label_by=c("Male", "Female"),
+   color = c("#2c7bb6","#b62ca1"),
+   digits=1) 
 
+dcasasr_male <- dcasasr[sex==1,]
+write.csv(dcasasr_male, "temp_color.csv")
 
-data
+color_test <- read.csv("temp_color.csv")
+dcasasr_male <- merge(dcasasr_male, color_test, by="LABEL")
+
+source("function.r")
+data <- csu_bar_top(
+   dcasasr_male,
+   var_top="asr",
+   var_bar="LABEL",
+   nb_top = 10,
+   plot_title = "Top 10 cancer sites",
+   ytitle= "Number of cases",
+   color="hex_color",
+   digits=1) 
+
+data <- csu_bar_top(
+   dcasasr_male,
+   var_top="pop",
+   var_bar="LABEL",
+   nb_top = 10,
+   plot_title = "Top 10 cancer sites",
+   ytitle= "Number of cases",
+   color= c("#2c7bb6","#b62ca1"),
+   digits=1) 
+
 ## add function barchart
 
 
