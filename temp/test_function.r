09d691602ddd145357fdea78f00f63c5bc24ba27
laversanne <timat35@gmail.com>
2019-06-05 14:53:19 +0200

    update doc

--- temp/test_function.r
+++ temp/test_function.r
@@ -28,7 +28,9 @@ test <- csu_registry_data_1[csu_registry_data_1$registry_label=="Colombia, Cali"
 
 # plot age specific rate for 1 population.
 csu_ageSpecific(test,
-        plot_title = "Colombia, Liver, male")
+        plot_title = "Colombia, Liver, male",
+        missing_age = 19
+        )
 
 # plot age specific rate for 1 population, and comparison with CI5X data.
 csu_ageSpecific(test,
@@ -54,7 +56,8 @@ d <- subset(dat, site == "Stomach" & sex == 1 & year == 2008)
 
 
 rate <- csu_ageSpecific(d, var_age = "age_group", var_cases = "cases", var_py = "py",
-                        group_by = "site", missing_age = 19, plot_title = "My registry: 2008-2012")
+                        group_by = "site", missing_age = 19, plot_title = "My registry: 2008-2012",
+                        CI5_comparison = "Stomach")
 
 rate <-as.data.table(rate)
 rate[nb_age_group == 16 & CSU_age_factor >= 16 , cases:=sum(cases), by="site"] ##add total_know
@@ -65,3 +68,11 @@ subset(dat, site=="Stomach" & sex == 1 & year == 2008)
 
 test <-  17 / 17025*100000
 test <-  (17+15+11) / 17025 * 100000
+
+#
+
+data("data_individual_file")
+d <- csu_group_cases(data_individual_file, var_age = "age", group_by = c("sex"), var_year = "doi")
+p <-  read.csv("Data_population-wide.csv")
+dp <- csu_merge_cases_pop(d, p, var_age = "age_group")
+
