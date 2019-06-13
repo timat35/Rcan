099a606ce8b781f1cc08cce9cbf02b95ff7f2825
laversanne <timat35@gmail.com>
2019-06-13 13:58:05 +0200

    add comment

--- Rcan/cran-comments.md
+++ Rcan/cran-comments.md
@@ -1,3 +1,8 @@
+## Apology
+I'm aware packages should be update no more than every 1-2 months. 
+I discovered after publication that the last update affect another software (CanReg5). It is my fault, I should I've checked carefully first.
+After checking with coworkers i've also fix other minor issues.
+
 ## Test environments
 * local windows 10 (devel and release)
 * win-builder (devel)
@@ -11,11 +16,3 @@ There were no ERRORs, WARNINGs or NOTEs.
 * Fix error in hidden function affecting CanReg5 software
 * Edit documentation
 * Add warnings
-
-## Comment
-I know package should be update no more than every 1-2 months. 
-I discovered after publication an update affect another software (CanReg5).
-It is my fault, I should I've checked carefully first.
-After checking with coworkers I've also fix other minor issues.
-Hopefully it will be possible to publish this update.
-
--- temp/test_function.r
+++ temp/test_function.r
@@ -13,22 +13,54 @@ library(ggplot2)
 
 setwd("C:/Projects/Rcan/temp")
 
-dsr <- read.csv("test.csv")
-csu_bar_top(dsr, var_value = "asr", var_bar = "site",
-            group_by = "sex", nb_top = 15,
-            plot_title = "Top 15 cancer sites",
-            xtitle = "Age-standardised rate per 100,000", 
-            color = c("#2c7bb6","#b62ca1"), 
-            label_by = c("Males", "Females"), digits = 1)
 
-Aucun souci.
-Mais si on remplace “asr” par “cases“
-csu_bar_top(dsr, var_value = "cases", var_bar = "site",
-            group_by = "sex", nb_top = 15,
-            plot_title = "Top 15 cancer sites",
-            xtitle = "Number of cases", 
-            color = c("#2c7bb6","#b62ca1"), 
-            label_by = c("Males", "Females"), digits = 0)
+dat <- read.csv(file="Exercise5.csv", header=TRUE)
+
+dat$year <-as.character(dat$year)
+
+
+csu_ageSpecific_top(dat, var_age = "age_group", var_cases = "cases", var_py = "py", nb_top = 5, 
+                    var_top = "site", group_by = "sex", missing_age = 19, plot_title = "My registry: 2008-2012")
+
+
+csu_ageSpecific(dat,
+            var_age="age_group",
+        var_cases="cases",
+        var_py="py",
+        group_by = "year", 
+        missing_age = 19,
+      ) 
+help(csu_ageSpecific_top)
+
+
+
+
+
+
+
+
+
+
+dasr <- csu_asr(dat, var_age = "age_group", var_cases = "cases", var_py = "py",
+                group_by = c("sex", "site"), missing_age = 19)
+dasr <- subset(dasr, site != "Other" & site != "Other skin")
+
+dasr <- dasr[order(dasr$sex, -dasr$asr),]
+
+par(mfrow=c(1,2))
+
+csu_bar_top(dasr[dasr$sex==1,],var_value = "asr", var_bar = "site", color="lightblue")
+
+csu_bar_top(dasr[dasr$sex==2,],var_value = "asr", var_bar = "site", color="tomato")
+
+csu_bar_top(dasr,var_value = "asr", var_bar = "site",group_by="sex",nb_top=15, 
+      color=c("lightblue", "tomato"),label_by=c("Males","Females"),
+      xtitle="ASR per 100,000")
+
+barplot(rev(dasr$asr[dasr$sex==1]), horiz = TRUE, col = "tomato", xlab = "ASR per 100,000",
+        names.arg = rev(dasr$site[dasr$sex==1]), las=2, main = "Males")
+barplot(rev(dasr$asr[dasr$sex==2]), horiz = TRUE, col = "lightblue", xlab = "ASR per 100,000",
+        names.arg = rev(dasr$site[dasr$sex==2]), las=2, main = "Females")
 
 
 
