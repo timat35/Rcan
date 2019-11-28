13675454b4f63856fb88d122aa3a1e3d28d9d921
laversanne <timat35@gmail.com>
2019-11-28 16:39:39 +0100

    after first check

--- Rcan/R/zzz.R
+++ Rcan/R/zzz.R
@@ -10,7 +10,7 @@
       c(
 		"CSU_A","CSU_BY","CSU_C","CSU_EAPC","CSU_LOW","CSU_P","CSU_RANK","CSU_ST","CSU_T",
 		"CSU_UP","CSU_Xaxes","CSU_Y","CSU_age_factor","CSU_age_label","CSU_cohort",
-		"CSU_dum_by","CSU_rate","CSU_smooth","age_factor","asr","correction","dummy_top",
+		"CSU_dum_by","CSU_rate","CSU_smooth","CSU_RESULT","age_factor","asr","correction","dummy_top",
 		"id_group","max_year","nb_age_group","pop15","pop16","pop17","rank_value","rate",
 		"smooth_value","st_err","temp","temp_age","temp_label","temp_label2","total",
 		"total_known","cases","ICD_group", "LABEL", "ICD", "age_group", "age_group_label", 
